12e85edc3831d1eb7a300315228819ef2f10a1c8
Blas Amarilla <bamarilla@reputationinstitute.com>
2020-08-29 15:40:07 -0300

    fixed image

--- src/components/MovieImage.js
+++ src/components/MovieImage.js
@@ -4,11 +4,7 @@ const MovieImage = (props) => {
     const imgPath = process.env.REACT_APP_IMAGE_PATH;
     const imgWidth = process.env.REACT_APP_IMAGE_STILL_SIZE;
 
-    return (
-        <div>
-            <img src={imgPath + imgWidth + props.movie.backdrop_path} alt={props.movie.title} onClick={props.redirectHandler} />
-        </div>
-    );
+    return <img className="is-rounded" src={imgPath + imgWidth + props.movie.backdrop_path} alt={props.movie.title} onClick={props.redirectHandler} />;
 }
 
 export default MovieImage;
