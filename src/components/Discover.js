2bebb6fbfeae23cadaa2dedd7a3306bccf27d0bf
Blas Amarilla <bamarilla@reputationinstitute.com>
2020-08-29 03:07:57 -0300

    added star filter

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -4,6 +4,7 @@ import MovieImageNotFound from "./MovieImageNotFound";
 
 const Discover = () => {
     const movies = useSelector(state => state.movies.movies);
+    const starFilter = useSelector(state => state.movies.starFilter);
 
     const imgPath = process.env.REACT_APP_IMAGE_PATH;
     const imgWidth = process.env.REACT_APP_IMAGE_STILL_SIZE;
@@ -12,17 +13,23 @@ const Discover = () => {
         <div className="Discover">
             <div className="columns is-multiline">
                 {
-                    movies.map( movie => {
-                        const img = movie.backdrop_path !== null
-                            ? <img src={imgPath + imgWidth + movie.backdrop_path} alt={movie.title} />
-                            : <MovieImageNotFound title={movie.title} average={movie.vote_average} />;
+                    movies
+                        .filter(movie => {
+                            return starFilter === 0 ||
+                                (movie.vote_average >= (starFilter - 1) * 2 &&
+                                (starFilter === 5 ? movie.vote_average <= starFilter * 2 : movie.vote_average < starFilter * 2));
+                        })
+                        .map(movie => {
+                            const img = movie.backdrop_path !== null
+                                ? <img src={imgPath + imgWidth + movie.backdrop_path} alt={movie.title} />
+                                : <MovieImageNotFound title={movie.title} average={movie.vote_average} />;
 
-                        return (
-                            <div className="column is-4 has-text-centered" key={movie.id}>
-                                <a href="#">{img}</a>
-                            </div>
-                        );
-                    })
+                            return (
+                                <div className="column is-4 has-text-centered" key={movie.id}>
+                                    <a href="#">{img}</a>
+                                </div>
+                            );
+                        })
                 }
             </div>
         </div>
--- src/store/reducers/movies.reducer.js
+++ src/store/reducers/movies.reducer.js
@@ -8,7 +8,7 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case movies.SET_MOVIES:
-            return { ...state, movies: action.movies }
+            return { ...state, movies: action.movies, starFilter: 0 }
         case movies.SET_STAR_FILTER:
             return { ...state, starFilter: action.starFilter }
         default:
